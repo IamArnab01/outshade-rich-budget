a934836a13314bcc6a13520a4f91f1d713da46a8
IamArnb01 <imarnab01@gmail.com>
2022-02-15 01:57:15 +0530

    chnages

--- components/home/plans.js
+++ components/home/plans.js
@@ -11,19 +11,21 @@ const HomePlans = () => {
     <div className="rb-home-plans-bg">
       <div className="rb-home-height">
         <div className="rb-container container">
-          <h1 className="rb-home-plan-heading pb-4">
+          <h1 className="rb-home-plan-heading pt-3 pb-5">
             Give wings to your finances. <br /> Plan smart.
           </h1>
           <div className="row pt-5 pb-3">
             <div className="col-md-3 d-flex justify-content-center">
               <div className="rb-home-plans-bg-card card">
-                <div className="d-flex justify-content-center">
-                  <Image src={Investment} alt="" className="w-100" />
+                <div>
+                  <div className="d-flex justify-content-center">
+                    <Image src={Investment} alt="" className="w-100" />
+                  </div>
+                  <div className="text-center">
+                    Investment <br /> Planning
+                  </div>
                 </div>
-                <div className="text-center">
-                  Investment <br /> Planning
-                </div>
-              </div>{" "}
+              </div>
             </div>
             <div className="col-md-3 d-flex justify-content-center">
               <div className="rb-home-plans-bg-card card">
